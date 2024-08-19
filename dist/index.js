fce52da953ab69039097641beba653957327ffcc
Tyler Smith <tyler@tksssoftware.com>
2024-08-19 10:16:12 -0300

    Remove dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Plugin_1 = require("./Plugin");
-exports.default = () => {
-    return new Plugin_1.Plugin();
-};
-//# sourceMappingURL=index.js.map
