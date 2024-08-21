4e6b8d836aa57d691bc49d41b092e445b392efa3
Tyler Smith <tyler@tksssoftware.com>
2024-08-21 13:16:16 -0300

    Filter out files and folders to exclude

--- src/findNodes.ts
+++ src/findNodes.ts
@@ -1,6 +1,8 @@
-import type { AstEditor, FunctionStatement, BrsFile, Program, TranspileObj, BscFile, Statement, Range } from 'brighterscript';
+import { match } from 'assert';
+import type { AstEditor, FunctionStatement, BrsFile, Program, TranspileObj, BscFile, Statement, Range, XmlScope } from 'brighterscript';
 import { isBrsFile, Parser, isXmlScope, DiagnosticSeverity, createVisitor, WalkMode, isDottedGetExpression, isVariableExpression, isLiteralString, util } from 'brighterscript';
 import type { SGNode } from 'brighterscript/dist/parser/SGTypes';
+import * as minimatch from 'minimatch';
 
 function findChildrenWithIDs(children: Array<SGNode>): Map<string, Range> {
     let foundIDs = new Map<string, Range>();
@@ -16,112 +18,133 @@ function findChildrenWithIDs(children: Array<SGNode>): Map<string, Range> {
     return foundIDs;
 }
 
+function getFilteredScopes(program: Program) {
+    const excludeScopes = program.options?.autoFindNode?.excludeFiles || [];
+    let scopes: XmlScope[] = [];
+
+    const hasExclusions = excludeScopes.length > 0;
+
+    for (const filteredScope of program.getScopes().filter((scope) => isXmlScope(scope))) {
+        if (!hasExclusions) {
+            scopes.push(filteredScope);
+        } else {
+            let isExcluded = excludeScopes.some((pattern: string) => 
+                minimatch.match([filteredScope.name], pattern).length > 0
+            );
+            if (!isExcluded) {
+                scopes.push(filteredScope);
+            }
+        }
+    }
+
+    return scopes;
+}
+
+
 export function findNodeWithIDInjection(program: Program, entries: TranspileObj[], editor: AstEditor, createdFiles: BscFile[]) {
-    for (const scope of program.getScopes()) {
-        if (isXmlScope(scope)) {
-            const xmlFile = scope.xmlFile;
-            const ids = findChildrenWithIDs(xmlFile.parser.ast.component?.children?.children ?? []);
-            if (ids.size > 0) {
-                const scopeFiles: BscFile[] = scope.getOwnFiles();
+    let scopes = getFilteredScopes(program);
+    for (const scope of scopes) {
+        const xmlFile = scope.xmlFile;
+        const ids = findChildrenWithIDs(xmlFile.parser.ast.component?.children?.children ?? []);
+        if (ids.size > 0) {
+            const scopeFiles: BscFile[] = scope.getOwnFiles();
 
-                //find an init function from all the scope's files
-                let initFunction: FunctionStatement | undefined;
+            //find an init function from all the scope's files
+            let initFunction: FunctionStatement | undefined;
 
-                let hasBrsFile = false;
-                for (const file of scopeFiles) {
-                    if (isBrsFile(file)) {
-                        hasBrsFile = true;
-                        initFunction = file.parser.references.functionStatementLookup.get('init');
-                        if (initFunction) {
-                            break;
-                        }
+            let hasBrsFile = false;
+            for (const file of scopeFiles) {
+                if (isBrsFile(file)) {
+                    hasBrsFile = true;
+                    initFunction = file.parser.references.functionStatementLookup.get('init');
+                    if (initFunction) {
+                        break;
                     }
                 }
+            }
 
-                if (!hasBrsFile) {
-                    createdFiles.push(program.setFile(xmlFile.pkgPath.replace('.xml', '.bs'), ''));
-                }
+            if (!hasBrsFile) {
+                createdFiles.push(program.setFile(xmlFile.pkgPath.replace('.xml', '.bs'), ''));
+            }
 
-                //create an init function if it's missing
-                if (!initFunction) {
-                    const codeBehindFile = program.getFiles<BrsFile>(xmlFile.possibleCodebehindPkgPaths).find(x => !!x);
-                    initFunction = Parser.parse(`sub init()\nend sub`).statements[0] as FunctionStatement;
-                    if (codeBehindFile) {
-                        editor.arrayPush(codeBehindFile.parser.statements, initFunction);
-                    }
+            //create an init function if it's missing
+            if (!initFunction) {
+                const codeBehindFile = program.getFiles<BrsFile>(xmlFile.possibleCodebehindPkgPaths).find(x => !!x);
+                initFunction = Parser.parse(`sub init()\nend sub`).statements[0] as FunctionStatement;
+                if (codeBehindFile) {
+                    editor.arrayPush(codeBehindFile.parser.statements, initFunction);
                 }
+            }
 
-                if (initFunction) {
-                    //add m variables for every xml component that has an id
-                    // eslint-disable-next-line max-statements-per-line, @typescript-eslint/brace-style
-                    const assignments = Array.from(ids).map(([id, range]) => { return `m.${id} = m.top.findNode("${id}")`; }).join('\n');
-                    const statements = (Parser.parse(`
-                        sub temp()
-                            ${assignments}
-                        end sub
-                    `).statements[0] as FunctionStatement).func.body.statements;
-                    //add the assignments to the top of the init function
-                    editor.arrayUnshift(initFunction.func.body.statements, ...statements);
-                }
+            if (initFunction) {
+                //add m variables for every xml component that has an id
+                // eslint-disable-next-line max-statements-per-line, @typescript-eslint/brace-style
+                const assignments = Array.from(ids).map(([id, range]) => { return `m.${id} = m.top.findNode("${id}")`; }).join('\n');
+                const statements = (Parser.parse(`
+                    sub temp()
+                        ${assignments}
+                    end sub
+                `).statements[0] as FunctionStatement).func.body.statements;
+                //add the assignments to the top of the init function
+                editor.arrayUnshift(initFunction.func.body.statements, ...statements);
             }
         }
     }
 }
 
 export function validateNodeWithIDInjection(program: Program) {
-    for (const scope of program.getScopes()) {
-        if (isXmlScope(scope)) {
-            const xmlFile = scope.xmlFile;
-            const ids = findChildrenWithIDs(xmlFile.parser.ast.component?.children?.children ?? []);
-            if (ids.size > 0) {
-                const scopeFiles: BscFile[] = scope.getOwnFiles();
+    let scopes = getFilteredScopes(program);
+    for (const scope of scopes) {
+        const xmlFile = scope.xmlFile;
+        const ids = findChildrenWithIDs(xmlFile.parser.ast.component?.children?.children ?? []);
+        if (ids.size > 0) {
+            const scopeFiles: BscFile[] = scope.getOwnFiles();
 
-                let initFunction: FunctionStatement | undefined;
-                let initFunctionFile: BscFile | undefined;
+            let initFunction: FunctionStatement | undefined;
+            let initFunctionFile: BscFile | undefined;
 
-                for (const file of scopeFiles) {
-                    if (isBrsFile(file)) {
-                        initFunction = file.parser.references.functionStatementLookup.get('init');
-                        if (initFunction) {
-                            initFunctionFile = file;
-                            break;
-                        }
+            for (const file of scopeFiles) {
+                if (isBrsFile(file)) {
+                    initFunction = file.parser.references.functionStatementLookup.get('init');
+                    if (initFunction) {
+                        initFunctionFile = file;
+                        break;
                     }
                 }
+            }
 
-                if (initFunction && initFunctionFile) {
-                    initFunction.func.body.walk(createVisitor({
-                        CallExpression: (expression) => {
-                            if (
-                                isDottedGetExpression(expression.callee) &&
-                                expression.callee.name.text.toLocaleLowerCase() === 'findnode' &&
-                                isDottedGetExpression(expression.callee.obj) &&
-                                expression.callee.obj.name.text.toLocaleLowerCase() === 'top' &&
-                                isVariableExpression(expression.callee.obj.obj) &&
-                                expression.callee.obj.obj.name.text.toLocaleLowerCase() === 'm' &&
-                                isLiteralString(expression.args[0])
-                            ) {
-                                let id = expression.args[0].token.text.replace(/^"/, '').replace(/"$/, '');
-                                let warningRange = ids.get(id);
-                                if (warningRange !== undefined) {
-                                    initFunctionFile!.diagnostics.push({
-                                        file: initFunctionFile!,
-                                        range: expression.range,
-                                        severity: DiagnosticSeverity.Warning,
-                                        message: `Unnecessary call to 'm.top.findNode("${id}")'`,
-                                        relatedInformation: [{
-                                            message: `In scope '${scope.name}'`,
-                                            location: util.createLocation(
-                                                util.pathToUri(xmlFile.srcPath),
-                                                warningRange
-                                            )
-                                        }]
-                                    });
-                                }
+            if (initFunction && initFunctionFile) {
+                initFunction.func.body.walk(createVisitor({
+                    CallExpression: (expression) => {
+                        if (
+                            isDottedGetExpression(expression.callee) &&
+                            expression.callee.name.text.toLocaleLowerCase() === 'findnode' &&
+                            isDottedGetExpression(expression.callee.obj) &&
+                            expression.callee.obj.name.text.toLocaleLowerCase() === 'top' &&
+                            isVariableExpression(expression.callee.obj.obj) &&
+                            expression.callee.obj.obj.name.text.toLocaleLowerCase() === 'm' &&
+                            isLiteralString(expression.args[0])
+                        ) {
+                            let id = expression.args[0].token.text.replace(/^"/, '').replace(/"$/, '');
+                            let warningRange = ids.get(id);
+                            if (warningRange !== undefined) {
+                                initFunctionFile!.diagnostics.push({
+                                    file: initFunctionFile!,
+                                    range: expression.range,
+                                    severity: DiagnosticSeverity.Warning,
+                                    message: `Unnecessary call to 'm.top.findNode("${id}")'`,
+                                    relatedInformation: [{
+                                        message: `In scope '${scope.name}'`,
+                                        location: util.createLocation(
+                                            util.pathToUri(xmlFile.srcPath),
+                                            warningRange
+                                        )
+                                    }]
+                                });
                             }
                         }
-                    }), { walkMode: WalkMode.visitExpressions });
-                }
+                    }
+                }), { walkMode: WalkMode.visitExpressions });
             }
         }
     }
